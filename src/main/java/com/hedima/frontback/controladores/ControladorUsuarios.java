37a89b44143c2cd5f239e7c0040ba2f496d9e3f6
TitaRuiz <ggp2002es@gmail.com>
2023-05-12 20:33:02 +0200

    Front - back

--- src/main/java/com/hedima/frontback/controladores/ControladorUsuarios.java
+++ src/main/java/com/hedima/frontback/controladores/ControladorUsuarios.java
@@ -1,2 +1,44 @@
-package com.hedima.frontback.controladores;public class ControladorUsuarios {
+package com.hedima.frontback.controladores;
+
+
+import com.hedima.frontback.modelo.Usuario;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ControladorUsuarios {
+
+    private List<Usuario> usuarios = new ArrayList<>();
+    private boolean primearavez = true;
+    @GetMapping("/usuarios")
+    public String mostrarUsuarios(Model modelo){
+        if(primearavez) {
+            usuarios.add(new Usuario(1, "Raul Gómez", "rg@gmail.com"));
+            usuarios.add(new Usuario(2, "Ana Flores", "af@gmail.com"));
+            usuarios.add(new Usuario(3, "Gonzalo Cruz", "gc@gmail.com"));
+            primearavez = false;
+        }
+        modelo.addAttribute("usuarios", usuarios);
+
+
+        return "usuarios";
+    }
+    @GetMapping("usuario/nuevo")
+    public String mostrarFormulario(Model modelo){
+        modelo.addAttribute("usuario",new Usuario());
+        return "formularioUsuario";
+    }
+
+    @PostMapping("usuario/usuario/guardar")
+    public String guardarFormulario(Usuario usuario, RedirectAttributes ra){
+        System.out.println(usuario.toString());
+        usuarios.add(usuario);
+        return "redirect:/usuarios";
+    }
 }
--- src/main/java/com/hedima/frontback/modelo/Usuario.java
+++ src/main/java/com/hedima/frontback/modelo/Usuario.java
@@ -1,2 +1,54 @@
-package com.hedima.frontback.modelo;public class Usuario {
+package com.hedima.frontback.modelo;
+
+public class Usuario {
+    //1. Atributos
+    private int id;
+    private String nombre;
+    private String email;
+
+    //2. Metodos
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    //3. Constructores
+    public Usuario()
+    {}
+    //4. Setters y Getters
+
+    public Usuario(int id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
